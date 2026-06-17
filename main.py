cdc7bfa194b7293df5a25b7da5e51c702a78df28
IAntigrippinI <kovalskii.bi@dvfu.ru>
2026-06-17 23:37:00 +1000

    Add init path before all imports for correct init configs

--- main.py
+++ main.py
@@ -3,15 +3,35 @@ import sys
 from PySide6.QtWidgets import QApplication
 from qfluentwidgets import Theme, setTheme
 
-from app.overlay_window import OverlayWindow
+from app.utils import start_up
+
+
 
 
 def main() -> int:
+    print("Initializing settings...")
+    start_up._init_path()
+
+    from app.config.windows import APP_NAME
+    from app.services.notification import NotificationService
+    from app.ui.windows.overlay_window import OverlayWindow
+
     app = QApplication(sys.argv)
+    app.setApplicationName(APP_NAME)
+
+    # Пока оставляем False, чтобы окно не закрывалось само из-за особенностей оверлея
+    app.setQuitOnLastWindowClosed(False)
 
     setTheme(Theme.DARK)
 
-    window = OverlayWindow()
+    notification_service = NotificationService(app)
+
+    window = OverlayWindow(notification_service=notification_service)
+
+    # Держим ссылки, чтобы Python не удалил объекты
+    app.window = window
+    app.notification_service = notification_service
+
     window.show()
 
     return app.exec()
